c4600fdaccef7562cc3e4436fef9019f6062369c
Langlois <llanglois@miiro.ca>
2026-05-19 10:05:21 +0200

    test: erreur lint volontaire (2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,22 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/__tests__/**/*.js", "**/*.test.js"], languageOptions: { globals: globals.jest } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-var": "error",
+      "no-unused-vars": "error"
+    }
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { sourceType: "commonjs" } 
+  },
+  { 
+    files: ["**/__tests__/**/*.js", "**/*.test.js"], 
+    languageOptions: { globals: globals.jest } 
+  },
 ]);
