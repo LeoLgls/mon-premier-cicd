2a05822cc3cb1d18783d45242a56dc4c841de32d
Langlois <llanglois@miiro.ca>
2026-05-19 10:28:58 +0200

    test: test version node volontairement cassé sur 20

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,12 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"], 
     plugins: { js }, 
     extends: ["js/recommended"], 
-    languageOptions: { globals: globals.browser },
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
     rules: {
       "no-var": "error",
       "no-unused-vars": "error"
@@ -15,18 +20,10 @@ export default defineConfig([
   },
   { 
     files: ["**/*.js"], 
-    languageOptions: { sourceType: "commonjs" },
-    rules: {
-      "no-var": "error",
-      "no-unused-vars": "error"
-    }
+    languageOptions: { sourceType: "commonjs" } 
   },
   { 
     files: ["**/__tests__/**/*.js", "**/*.test.js"], 
-    languageOptions: { globals: globals.jest },
-    rules: {
-      "no-var": "error",
-      "no-unused-vars": "error"
-    }
+    languageOptions: { globals: globals.jest } 
   },
 ]);
